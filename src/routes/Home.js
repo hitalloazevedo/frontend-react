237250a83bc37527d7d4af31fc4d5c191e5a58b8
HitalloAzevedo <azevedohitallo@gmail.com>
2023-12-01 19:38:01 -0300

    add link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     const navigate = useNavigate()
     return(
         <>
-            <a href={navigate('/login')}>login</a>
+            <Link to={'/login'}>login</Link>
         </>
     )
 }
