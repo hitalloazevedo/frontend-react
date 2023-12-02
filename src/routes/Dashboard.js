296b2cef68ea446889a50d67201fbe63ef347ce6
HitalloAzevedo <azevedohitallo@gmail.com>
2023-12-01 23:24:55 -0300

    new styles

--- src/routes/Dashboard.js
+++ src/routes/Dashboard.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import jwt from "jsonwebtoken"
 import RenderProducts from "../components/RenderProducts.js"
 import Header from "../components/Header.js"
+import '../assets/css/Dashboard.css'
 
 export default function Dashboard() {
 
@@ -13,8 +14,10 @@ export default function Dashboard() {
     return (
         <>
             <Header></Header>
-            <h1>Dashboard</h1>
-            <RenderProducts/>
+            <div class="main">
+                <h1 className="title">Dashboard</h1>
+                <RenderProducts/>
+            </div>
         </>
     )
 }
